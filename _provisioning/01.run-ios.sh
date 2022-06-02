f340bee37690a66f387c82accb33b3002c1f1fda
shawn.kim <metocks@gmail.com>
2022-06-02 17:23:37 +0900

    export nvm environment variables

--- _provisioning/01.run-ios.sh
+++ _provisioning/01.run-ios.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 __UNAME_MACHINE="$(/usr/bin/uname -m)"
 export UNAME_MACHINE="${__UNAME_MACHINE}"
 if [[ "$UNAME_MACHINE" == "arm64" ]]; then
