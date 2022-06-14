32463997f01f56a9b4869241911c8365e3a64777
shawn.kim <metocks@gmail.com>
2022-06-14 14:30:04 +0900

    hotfix

--- _provisioning/01.run-ios.sh
+++ _provisioning/01.run-ios.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-__UNAME_MACHINE="$(/usr/bin/uname -m)"
-export UNAME_MACHINE="${__UNAME_MACHINE}"
-if [[ "$UNAME_MACHINE" == "arm64" ]]; then
-  HOMEBREW_PREFIX="/opt/homebrew"
-  export PATH="${HOMEBREW_PREFIX}/bin:$PATH"
-else
-  HOMEBREW_PREFIX="/usr/local"
-  export PATH="${HOMEBREW_PREFIX}/bin:$PATH"
-fi
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 PATH_TO_OPT='/opt/ramiel/ws-scrcpy-ios'
 cd "${PATH_TO_OPT}" || exit 1
