e7f131469aa7deb91e625e384f3f62f66d116f7c
metocks <metocks@users.noreply.github.com>
2022-08-17 15:41:20 +0900

    refactoring: remove `~Async` naming

--- src/server/mw/MjpegProxyFactory.ts
+++ src/server/mw/MjpegProxyFactory.ts
@@ -17,7 +17,7 @@ export class MjpegProxyFactory {
         const wda = await WdaRunner.getInstance(udid);
         while (!wda.isStarted() && ii < 10) {
             ii += 1;
-            await Utils.sleepAsync(1000);
+            await Utils.sleep(1000);
         }
         if (!wda.isStarted()) {
             wda.emit('status-change', { status: WdaStatus.ERROR, text: '대기 시간 초과: 화면 전송 세션' });
